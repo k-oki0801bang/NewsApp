fc30109e2ed166bac8052b32ad36e406be36127b
Kouki <yamadakoki0124@icloud.com>
2022-04-05 21:31:57 +0900

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by 山田滉暁 on 2022/04/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
