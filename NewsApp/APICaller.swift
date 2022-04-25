5750ab188c8d28fadfd736e3742afc47e35bb6ab
Kouki <yamadakoki0124@icloud.com>
2022-04-25 17:05:39 +0900

    APIのロジックをまとめる

---
+++ NewsApp/APICaller.swift
@@ -0,0 +1,61 @@
+//
+//  APICaller.swift
+//  NewsApp
+//
+//  Created by 山田滉暁 on 2022/04/05.
+//
+
+import Foundation
+
+final class APICaller {
+    static let shared = APICaller()
+    
+    struct Constants {
+        static let topHeadlinesURL = URL(string: "https://newsapi.org/v2/top-headlines?country=jp&category=technology&apiKey=8bbc1647d807474eb0d0e8a4661d70fa")
+    }
+    
+    private init() {}
+    
+    public func getTopStories(completion: @escaping (Result<[Article], Error>) -> Void) {
+        guard let url = Constants.topHeadlinesURL else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            else if let data = data {
+                
+                do {
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                    
+                    print("Articles: \(result.articles.count)")
+                    completion(.success(result.articles))
+                }
+                catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+        task.resume()
+    }
+}
+
+//Models
+struct APIResponse: Codable {
+    let articles: [Article]
+}
+
+struct Article: Codable {
+    let source: Source
+    let title: String
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String
+}
+
+struct Source: Codable {
+    let name: String
+}
